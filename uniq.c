5cc29a7674994f2378070f2878d22717ba8f3f29
anubis-robot <anubis@osiris.cyber.nyu.edu>
2021-10-07 01:55:13 +0000

    Anubis Cloud IDE Autosave

--- uniq.c
+++ uniq.c
@@ -4,37 +4,37 @@
 
 char buf[512];
 
-// int strcmp(const char *X, const char *Y) {
-//   while (*X) {
-//     if (*X != *Y) {
-//       break;
-//     }
-//     X++;
-//     Y++;
-//   }
-//   return *(const unsigned char *)X - *(const unsigned char *)Y;
-// }
+int strcmp(const char *X, const char *Y) {
+  while (*X) {
+    if (*X != *Y) {
+      break;
+    }
+    X++;
+    Y++;
+  }
+  return *(const unsigned char *)X - *(const unsigned char *)Y;
+}
 
-// void uniq(int fd) {
-//   int i, n;
+void uniq(int fd) {
+  int i, n;
 //   int *start;
 //   char *prev;
-//   while ((n = read(fd, buf, sizeof(buf))) > 0) {
-//     for (i = 0; i < n; i++) {
-//         printf(1, "%s", buf[i]);
-//     //   if (buf[i] == '\n') {
+  while ((n = read(fd, buf, sizeof(buf))) > 0) {
+    for (i = 0; i < n; i++) {
+        printf(1, "This is the %dth string: %s",i, buf[i]);
+    //   if (buf[i] == '\n') {
 
-//     //   } else {
-//     //   }
-//     }
-//   }
-//   if (n < 0) {
-//     printf(1, "wc: read error\n");
-//     exit();
-//   }
-// }
+    //   } else {
+    //   }
+    }
+  }
+  if (n < 0) {
+    printf(1, "wc: read error\n");
+    exit();
+  }
+}
 
-int sys_uniq(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   int fd, i;
 
   // read from standard input
