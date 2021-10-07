0dfd50537634f7f48c9e3fcb7cf34abb38b48edb
anubis-robot <anubis@osiris.cyber.nyu.edu>
2021-10-07 01:25:07 +0000

    Anubis Cloud IDE Autosave

--- uniq.c
+++ uniq.c
@@ -4,46 +4,52 @@
 
 char buf[512];
 
-void uniq(int fd, char *name) {
-  int i, n;
-  int l, w, c, inword;
+int strcmp(const char *X, const char *Y) {
+  while (*X) {
+    if (*X != *Y) {
+      break;
+    }
+    X++;
+    Y++;
+  }
+  return *(const unsigned char *)X - *(const unsigned char *)Y;
+}
 
-  l = w = c = 0;
-  inword = 0;
+void uniq(int fd) {
+  int i, n;
+  int *start;
+  char *prev;
   while ((n = read(fd, buf, sizeof(buf))) > 0) {
     for (i = 0; i < n; i++) {
-      c++;
-      if (buf[i] == '\n')
-        l++;
-      if (strchr(" \r\t\n\v", buf[i]))
-        inword = 0;
-      else if (!inword) {
-        w++;
-        inword = 1;
-      }
+        printf(1, "%s", buf[i]);
+    //   if (buf[i] == '\n') {
+
+    //   } else {
+    //   }
     }
   }
   if (n < 0) {
     printf(1, "wc: read error\n");
     exit();
   }
-  printf(1, "%d %d %d %s\n", l, w, c, name);
+  printf(1, "%d %d %d %s\n", l, w, c);
 }
 
 int main(int argc, char *argv[]) {
   int fd, i;
 
+  // read from standard input
   if (argc <= 1) {
-    uniq(0, "");
+    uniq(0);
     exit();
   }
-
+  // read from file passed as arguement
   for (i = 1; i < argc; i++) {
     if ((fd = open(argv[i], 0)) < 0) {
       printf(1, "wc: cannot open %s\n", argv[i]);
       exit();
     }
-    uniq(fd, argv[i]);
+    uniq(fd);
     close(fd);
   }
   exit();
