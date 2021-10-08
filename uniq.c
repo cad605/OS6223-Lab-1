5f5fd96cd5eaae8112b6406d0867d782e597251c
anubis-robot <anubis@osiris.cyber.nyu.edu>
2021-10-08 18:37:55 +0000

    Anubis Cloud IDE Autosave

--- uniq.c
+++ uniq.c
@@ -3,8 +3,9 @@
 #include "user.h"
 
 char buf[512];
+char delimiter = '\n';
 
-void uniq(int fd) {
+void uniq(int fd, char delimiter) {
   int n;
 
   while ((n = read(fd, buf, sizeof(buf))) > 0)
@@ -16,21 +17,19 @@ void uniq(int fd) {
 }
 
 int main(int argc, char *argv[]) {
-//   char delimiter = '\n';
   int fd, i;
 
-  // read from standard input
   if (argc <= 1) {
-    uniq(0);
+    uniq(0, delimiter);
     exit();
   }
-  // read from file passed as arguement
+
   for (i = 1; i < argc; i++) {
     if ((fd = open(argv[i], 0)) < 0) {
-      printf(1, "wc: cannot open %s\n", argv[i]);
+      printf(1, "cat: cannot open %s\n", argv[i]);
       exit();
     }
-    uniq(fd);
+    uniq(fd, delimiter);
     close(fd);
   }
   exit();
