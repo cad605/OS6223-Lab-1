b1d6f2d888a0489a75543756bd3f1f57233d47fd
anubis-robot <anubis@osiris.cyber.nyu.edu>
2021-10-08 19:13:00 +0000

    Anubis Cloud IDE Autosave

--- uniq.c
+++ uniq.c
@@ -5,11 +5,11 @@
 
 char buf[512];
 char delimiter = '\n';
-static bool output_unique;
+static int count_group, dups_only, ignore_case = 0;
 
 /* When fed an input, outputs the input with adjacent identical lines collapsed
  * to one*/
-void uniq(int fd, char delimiter) {
+void uniq(int fd) {
   int i, n, lines = 0;
 
   while ((n = read(fd, buf, sizeof(buf))) > 0) {
@@ -35,12 +35,15 @@ int main(int argc, char *argv[]) {
   while (--argc > 0 && (*++argv)[0] == '-') {
     while (c = *++argv[0]) {
       switch (c) {
-      // count and group prefix lines by the number of occurrences
+      // count, group, and prefix lines by the number of occurrences
       case 'c':
+        count_group = 1;
       // only print duplicate lines
       case 'd':
+        dups_only = 1;
       // ignore differences in case when comparing
       case 'i':
+        ignore_case = 1;
       default:
         printf(1, "uniq: illegal option %c\n", c);
         argc = 0;
@@ -49,7 +52,7 @@ int main(int argc, char *argv[]) {
     }
   }
   if (argc <= 1) {
-    uniq(0, delimiter);
+    uniq(0);
     exit();
   }
 
@@ -58,7 +61,7 @@ int main(int argc, char *argv[]) {
       printf(1, "uniq: cannot open %s\n", argv[i]);
       exit();
     }
-    uniq(fd, delimiter);
+    uniq(fd);
     close(fd);
   }
   exit();
